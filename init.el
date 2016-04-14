6dd02ecefe0fe208ceb0c4b544f0facdc137ea6d
Kristoffer Dalby <kradalby@kradalby.no>
2016-04-14 21:25:23 +0200

    adds file dedicated to keybindings

--- init.el
+++ init.el
@@ -1,11 +1,10 @@
-;; Turn off toolbar and scroll
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+;; Turn off toolbar and scroll
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
@@ -85,6 +84,9 @@
 
 ;; Map the different modes
 (require 'mode-map)
+
+;; Key-bindings
+(require 'key-bindings)
 
 ;; Setup environment variables from the user's shell on Mac.
 (when is-mac
---
+++ settings/key-bindings.el
@@ -0,0 +1,8 @@
+
+
+;; Magit
+(global-set-key (kbd "C-x m") 'magit-status-fullscreen)
+(autoload 'magit-status-fullscreen "magit")
+
+(provide 'key-bindings)
+
